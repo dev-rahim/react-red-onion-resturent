1cfae0caf54e0667d82d3dbd3d5c8a0823b31156
dev-rahim <rahimuddin5915@gmail.com>
2022-12-02 12:30:24 +0600

    items section added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,9 +9,9 @@ const Header = () => {
             <div className="align-items-center header-banner">
                 <div className="header-content pt-5 d-flex justify-content-center container flex-column align-items-center">
                     <h1 className='pt-5'>Best Food is waiting for your belly</h1>
-                    <div class="input-group mb-3 mt-3 w-50">
-                        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+                    <div className="input-group mb-3 mt-3 w-50">
+                        <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                        <button className="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
                     </div>
                 </div>
             </div>
--- src/Components/ItemesSection/ItemsSection.js
+++ src/Components/ItemesSection/ItemsSection.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ListGroup } from 'reactstrap';
 import './ItemsSection.css'
 const ItemsSection = () => {
+    const [lunchItems, setLunchItems] = useState([]);
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/dev-rahim/react-red-onion-resturent/main/src/Fakedata/menusfakedata/lunch.json')
+            .then(res => res.json())
+            .then(data => setLunchItems(data))
+    }, [])
+    console.log(lunchItems);
     return (
         <div className=''>
             <div className="container">
@@ -9,7 +17,19 @@ const ItemsSection = () => {
                     <button className='items-btn items-active'>Lunch</button>
                     <button className='items-btn'>Dinner</button>
                 </div>
-                
+                <div className="container items-container">
+                    <div className="row">
+                        {
+                            lunchItems.map(lunchItem => <div className=' col-lg-4 col-xl-4'>
+                                <div className="lunch-item w-100 p-3 m-3">
+                                    <img src={lunchItem.image} alt="" />
+                                    <h4>{lunchItem.name}</h4>
+                                    <p>{lunchItem.description}</p>
+                                </div>
+                            </div>)
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     );
